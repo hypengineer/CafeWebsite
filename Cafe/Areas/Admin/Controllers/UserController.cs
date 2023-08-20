781165f15c44326c2137910aae59a3ab4d3fb42a
Hypengineer <okanizi16@gmail.com>
2023-08-20 21:54:06 +0300

    User Controller Oluşturma

---
+++ Cafe/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Cafe.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language;
+using System.Linq;
+
+namespace Cafe.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var users = _context.ApplicationUsers.ToList();
+            var role=_context.Roles.ToList();
+            var userRol=_context.UserRoles.ToList();
+            foreach (var item in users)
+            {
+                var roleId = userRol.FirstOrDefault(i => i.UserId == item.Id).RoleId;
+                item.Role = role.FirstOrDefault(u => u.Id == roleId).Name;
+            }
+            return View(users);
+        }
+    }
+}
